1246b439a84066fd07616f0a652a5c9595c4e0a9
chinguyen21 <nguyen.thimai@northeastern.edu>
2020-11-17 22:53:54 -0500

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,38 @@
+class Song
+  attr_reader :name, :artist, :genre
+  @@count = 0
+  @@artists = []
+  @@genres = []
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@count += 1
+    @@artists << artist
+    @@genres << genre
+  end
+
+  def self.count
+    @@count
+  end
+
+  def self.artists
+    @@artists.uniq
+  end
+
+  def self.genres
+    @@genres.uniq
+  end
+
+  def self.genre_count
+    hash = Hash.new(0)
+    @@genres.each {|word| hash[word]+=1}
+    hash
+  end
+
+  def self.artist_count
+    hash = Hash.new(0)
+    @@artists.each {|word| hash[word]+=1}
+    hash
+  end
+end
